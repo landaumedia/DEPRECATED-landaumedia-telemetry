8305e56831d0c79918a2bd823237aa65275c3823
Steve Wagner <wagner@landaumedia.de>
2017-06-27 15:33:35 +0200

    Inc version to 1.1.5

--- Source/LandauMedia.Telemetry/Properties/AssemblyInfo.cs
+++ Source/LandauMedia.Telemetry/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("LandauMedia.Telemetry")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d2085537-c0e1-40b1-b249-5c6a725bae67")]
-[assembly: AssemblyVersion("1.1.4")]
-[assembly: AssemblyFileVersion("1.1.4")]
+[assembly: AssemblyVersion("1.1.5")]
+[assembly: AssemblyFileVersion("1.1.5")]
